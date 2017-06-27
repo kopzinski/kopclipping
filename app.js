c55f23c94272a8e47e145a057e7a5ef3239a39e5
Paulo Mello <eduardo@kopzinski.com.br>
2017-06-26 22:24:18 -0300

    saving each tweet

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var TwitterPackage = require('twitter');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
 
 var secret = {
   consumer_key: 'EyYjCYULYD1fsC9MaTkcYIXvd',
@@ -7,14 +10,30 @@ var secret = {
   access_token_secret: 'bLGBuG5pZCYl2mhjVAUcKWFVUVCK0fKg7IQGLzaMDvpAe'
 }
 
+var Post = mongoose.model('Post', { text: String });
+
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', {track: '@AntonioVSCastro'}, function(stream) {
+Twitter.stream('statuses/filter', {track: '@realDonaldTrump'}, function(stream) {
+
   stream.on('data', function(tweet) {
-    console.log(tweet.text);
+
+    console.log( tweet.text );
+
+    var postAtual = new Post( { text: tweet.text } );
+
+    postAtual.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Salvo com sucesso.');
+      }
+    });
+
   });
 
   stream.on('error', function(error) {
     console.log(error);
   });
+
 });
